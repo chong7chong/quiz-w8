d4906493c0430a7d6e7ea1ad4d876e576b73f2c0
chong7chong <guozeng.rong@163.com>
2018-08-17 22:20:09 +0800

    first commit

--- research/run.sh
+++ research/run.sh
@@ -13,7 +13,7 @@ output_dir=/output  # 训练目录
 dataset_dir=/data/chong77chong/quiz-w8 # 数据集目录，这里是写死的，记得修改
 
 train_dir=$output_dir/train
-checkpoint_dir=$train_dir
+checkpoint_dir=$dataset_dir
 eval_dir=$output_dir/eval
 
 # config文件
@@ -42,7 +42,7 @@ do
 done
 
 # 导出模型
-python ./object_detection/export_inference_graph.py --input_type image_tensor --pipeline_config_path $pipeline_config_path --trained_checkpoint_prefix $train_dir/model.ckpt-$current  --output_directory $output_dir/exported_graphs
+python ./object_detection/export_inference_graph.py --input_type image_tensor --pipeline_config_path $pipeline_config_path --trained_checkpoint_prefix $dataset_dir/model.ckpt-$current  --output_directory $output_dir 
 
 # 在test.jpg上验证导出的模型
 python ./inference.py --output_dir=$output_dir --dataset_dir=$dataset_dir
